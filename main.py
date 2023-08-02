6b8059360e8513ed5d970e20f98cee9fce04ea8a
diwas-stha <diwas@fusemachines.com>
2023-08-02 15:24:09 +0545

    return sum, difference and product of two numeric inputs

---
+++ main.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+def addition():
+    return num1 + num2
+
+def subtraction():
+    return num1 - num2
+
+def multiplication():
+    return num1 * num2
+
+
+print("The sum is:", addition())
+print("The difference is:", subtraction())
+print("The product is:", multiplication())
